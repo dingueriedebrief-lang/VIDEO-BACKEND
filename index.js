e7fb703e6403ab412fa0cd4432df02dd0bc324d1
ODZAME MEZUI P.S.SMITH <dingueriedebrief@gmail.com>
2026-05-03 07:42:18 +0100

    fix port railway

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("API is working 🚀");
 });
+const PORT = process.env.PORT;
 
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
 app.listen(process.env.PORT, () => {
   console.log("Server running on port " + process.env.PORT);
 });
