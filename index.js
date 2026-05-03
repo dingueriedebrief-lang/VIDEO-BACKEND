01df6958d99c011233267707cb087b74116f7cc7
ODZAME MEZUI P.S.SMITH <dingueriedebrief@gmail.com>
2026-05-03 08:01:51 +0100

    force root route

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ app.get("/", (req, res) => {
   res.send("API is working 🚀");
 });
 
-app.listen(process.env.PORT, () => {
-  console.log("Server running on port " + process.env.PORT);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("Server running on port " + PORT);
 });
