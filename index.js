01616232db4ed648b086e529078b7d5057020df0
ODZAME MEZUI P.S.SMITH <dingueriedebrief@gmail.com>
2026-05-03 07:50:44 +0100

    add root route

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
 const express = require("express");
-
 const app = express();
 
-// route test
 app.get("/", (req, res) => {
   res.send("API is working 🚀");
 });
-const PORT = process.env.PORT;
 
-app.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
-});
 app.listen(process.env.PORT, () => {
   console.log("Server running on port " + process.env.PORT);
 });
