dee457df2424434f712c5fe62f8b61aca7d5e044
ODZAME MEZUI P.S.SMITH <dingueriedebrief@gmail.com>
2026-05-03 07:22:45 +0100

    remove ffmpeg test

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const express = require("express");
-const multer = require("multer");
-const express = require("express");
 
 const app = express();
 
@@ -11,32 +9,4 @@ app.get("/", (req, res) => {
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("Server running");
-});
-
-const app = express();
-app.get("/", (req, res) => {
-  res.send("API is working 🚀");
-});
-const upload = multer({ dest: "uploads/" });
-
-app.post("/clip", upload.single("video"), (req, res) => {
-  const inputPath = req.file.path;
-  const outputPath = `output-${Date.now()}.mp4`;
-
-  ffmpeg(inputPath)
-    .setStartTime(0)
-    .setDuration(30)
-    .output(outputPath)
-    .on("end", () => {
-      res.download(outputPath);
-    })
-    .on("error", (err) => {
-      console.error(err);
-      res.status(500).send("Erreur");
-    })
-    .run();
-});
-
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Server running on port " + (process.env.PORT || 3000));
 });
