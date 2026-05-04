be85a9130325f5b406eecd3936e00a6499edb564
ODZAME MEZUI P.S.SMITH <dingueriedebrief@gmail.com>
2026-05-04 05:42:20 +0100

    fix cors

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ const app = express();
 // =====================
 // 🔧 MIDDLEWARES
 // =====================
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST"],
+  allowedHeaders: ["Content-Type"]
+}));
 app.use(express.json());
 
 // =====================
